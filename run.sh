a3faa23b7dc4530418e3be75820f00a6663c2e7a
probinso <pmoss.robinson@gmail.com>
2016-04-03 22:48:56 -0700

    explicit commenting

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+<<COMMENT
 if (( $# != 2)); then
   printf "Usage: %s input_path output_path \n" "$0" >&2 ;
   exit 1;
 fi;
+COMMENT
 
 python3 ./src/challenge-runner.py rawtweet ./tweet_input/tweets.txt ./tweet_output/output.txt
 
